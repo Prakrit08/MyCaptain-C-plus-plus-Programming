34dc0454dd8523304d8816e8a393ba84356fb97a
PRAKRIT RAJ <63188483+Prakrit08@users.noreply.github.com>
2020-05-04 13:08:37 +0530

    Update Votetest.cpp

--- Votetest.cpp
+++ Votetest.cpp
@@ -1,2 +1,21 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+	do
+	{
+		int a,f=0;
+		cout<<"Enter your age : "; 
+		cin>>a;
+		if(a>=18)
+			cout<<"You are eligible!!";
+		else if(a>0 && a<18)
+			cout<<"You are not eligible!!";
+		else
+		{
+			cout<<"Please enter your age properly!!";
+			f=1;
+		}
+	}while(f==1);
+}
+	
